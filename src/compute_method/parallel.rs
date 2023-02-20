cfe69e0deaf7e6f4eee121d3adf8c53170123bbc
Canleskis <91223490+Canleskis@users.noreply.github.com>
2023-02-20 07:02:04 +0100

    Add brute-force `ComputeMethod` for all floats

--- src/compute_method/parallel.rs
+++ src/compute_method/parallel.rs
@@ -1,38 +1,48 @@
 use crate::compute_method::ComputeMethod;
 
-use glam::Vec3A;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 /// A brute-force [`ComputeMethod`] using the CPU with [rayon](https://github.com/rayon-rs/rayon).
 pub struct BruteForce;
 
-impl ComputeMethod<Vec3A, f32> for BruteForce {
-    #[inline]
-    fn compute(&mut self, massive: Vec<(Vec3A, f32)>, massless: Vec<(Vec3A, f32)>) -> Vec<Vec3A> {
-        let massive_len = massive.len();
-
-        let particles = &[massive, massless].concat()[..];
-        let len = particles.len();
-
-        (0..len)
-            .into_par_iter()
-            .map(|i| {
-                let mut acceleration = Vec3A::ZERO;
-
-                for j in 0..massive_len {
-                    if i != j {
-                        let dir = particles[j].0 - particles[i].0;
-                        let mag_2 = dir.length_squared();
-
-                        acceleration += dir * particles[j].1 / (mag_2 * mag_2.sqrt())
-                    }
-                }
-
-                acceleration
-            })
-            .collect()
+macro_rules! impl_brute_force {
+    ($scalar: ty, $internal: ty) => {
+        impl ComputeMethod<$internal, $scalar> for BruteForce {
+            #[inline]
+            fn compute(&mut self, massive: Vec<($internal, $scalar)>, massless: Vec<($internal, $scalar)>) -> Vec<$internal> {
+                let massive_len = massive.len();
+        
+                let particles = &[massive, massless].concat()[..];
+                let len = particles.len();
+        
+                (0..len)
+                    .into_par_iter()
+                    .map(|i| {
+                        let mut acceleration = <$internal>::ZERO;
+        
+                        for j in 0..massive_len {
+                            if i != j {
+                                let dir = particles[j].0 - particles[i].0;
+                                let mag_2 = dir.length_squared();
+        
+                                acceleration += dir * particles[j].1 / (mag_2 * mag_2.sqrt())
+                            }
+                        }
+        
+                        acceleration
+                    })
+                    .collect()
+            }
+        }
     }
 }
+
+impl_brute_force!(f32, glam::Vec3A);
+impl_brute_force!(f32, glam::Vec4);
+
+impl_brute_force!(f64, glam::DVec2);
+impl_brute_force!(f64, glam::DVec3);
+impl_brute_force!(f64, glam::DVec4);
 
 #[cfg(test)]
 mod tests {
--- src/compute_method/sequential.rs
+++ src/compute_method/sequential.rs
@@ -1,42 +1,55 @@
 use crate::compute_method::ComputeMethod;
 
-use glam::Vec3A;
-
 /// A brute-force [`ComputeMethod`] using the CPU.
 pub struct BruteForce;
 
-impl ComputeMethod<Vec3A, f32> for BruteForce {
-    #[inline]
-    fn compute(&mut self, massive: Vec<(Vec3A, f32)>, massless: Vec<(Vec3A, f32)>) -> Vec<Vec3A> {
-        let massive_len = massive.len();
+macro_rules! impl_brute_force {
+    ($scalar: ty, $internal: ty) => {
+        impl ComputeMethod<$internal, $scalar> for BruteForce {
+            #[inline]
+            fn compute(
+                &mut self,
+                massive: Vec<($internal, $scalar)>,
+                massless: Vec<($internal, $scalar)>,
+            ) -> Vec<$internal> {
+                let massive_len = massive.len();
 
-        let particles = &[massive, massless].concat()[..];
-        let len = particles.len();
+                let particles = &[massive, massless].concat()[..];
+                let len = particles.len();
 
-        let mut accelerations = vec![Vec3A::ZERO; len];
+                let mut accelerations = vec![<$internal>::ZERO; len];
 
-        for i in 0..massive_len {
-            let (pos1, mu1) = particles[i];
-            let mut acceleration = Vec3A::ZERO;
+                for i in 0..massive_len {
+                    let (pos1, mu1) = particles[i];
+                    let mut acceleration = <$internal>::ZERO;
 
-            for j in (i + 1)..len {
-                let (pos2, mu2) = particles[j];
+                    for j in (i + 1)..len {
+                        let (pos2, mu2) = particles[j];
 
-                let dir = pos2 - pos1;
-                let mag_2 = dir.length_squared();
+                        let dir = pos2 - pos1;
+                        let mag_2 = dir.length_squared();
 
-                let f = dir / (mag_2 * mag_2.sqrt());
+                        let f = dir / (mag_2 * mag_2.sqrt());
 
-                acceleration += f * mu2;
-                accelerations[j] -= f * mu1;
+                        acceleration += f * mu2;
+                        accelerations[j] -= f * mu1;
+                    }
+
+                    accelerations[i] += acceleration;
+                }
+
+                accelerations
             }
-
-            accelerations[i] += acceleration;
         }
-
-        accelerations
-    }
+    };
 }
+
+impl_brute_force!(f32, glam::Vec3A);
+impl_brute_force!(f32, glam::Vec4);
+
+impl_brute_force!(f64, glam::DVec2);
+impl_brute_force!(f64, glam::DVec3);
+impl_brute_force!(f64, glam::DVec4);
 
 #[cfg(test)]
 mod tests {
