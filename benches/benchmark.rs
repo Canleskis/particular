16387d84a8fc344cd9676c3603506a8743cc631b
Canleskis <91223490+Canleskis@users.noreply.github.com>
2023-07-09 03:06:33 +0200

    Update benchmark and graph

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use criterion::{AxisScale, BenchmarkGroup, BenchmarkId, Criterion, PlotConfiguration};
 
 use glam::Vec3 as Vector;
@@ -28,14 +30,22 @@ pub fn random_bodies(mut rng: StdRng, i: usize) -> Vec<Body> {
         .collect()
 }
 
-fn bench_compute_method<M, S, C>(bodies: &[Body], group: &mut BenchmarkGroup<'_, M>, mut cm: C)
-where
+fn bench_compute_method<M, S, C>(
+    bodies: &[Body],
+    group: &mut BenchmarkGroup<'_, M>,
+    mut cm: C,
+    suffix: &str,
+) where
     M: criterion::measurement::Measurement,
-    S: compute_method::Storage<PointMass<Body>>,
-    for<'a> &'a mut C: compute_method::ComputeMethod<S, Vector>,
+    S: Storage<ParticlePointMass<Body>>,
+    for<'a> &'a mut C: ComputeMethod<S, Vector>,
 {
     let name = std::any::type_name::<C>().trim_start_matches("particular::algorithms::");
-    let bench_name = name.find('<').map_or(name, |index| &name[..index]);
+    let bench_name = name
+        .find('<')
+        .map_or(name, |index| &name[..index])
+        .to_owned()
+        + suffix;
 
     group.bench_function(BenchmarkId::new(bench_name, bodies.len()), |b| {
         b.iter(|| bodies.iter().accelerations(&mut cm).collect::<Vec<_>>())
@@ -43,39 +53,39 @@ where
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Particular");
-    group
-        .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic))
-        .warm_up_time(std::time::Duration::from_secs(1))
+    let mut gr = c.benchmark_group("Particular");
+    gr.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic))
+        .warm_up_time(std::time::Duration::from_secs(2))
+        .measurement_time(Duration::from_secs(4))
         .sample_size(50);
 
-    let particle_count_iterator = (0..16).map(|i| 2usize.pow(i));
+    let particle_count_iterator = (1..16).map(|i| 2usize.pow(i));
 
     for i in particle_count_iterator {
-        let bodies = random_bodies(StdRng::seed_from_u64(1808), i);
+        let b = random_bodies(StdRng::seed_from_u64(1808), i);
 
         #[cfg(feature = "gpu")]
         {
-            bench_compute_method(&bodies, &mut group, gpu::BruteForce::new_init(i, i));
+            bench_compute_method(&b, &mut gr, gpu::BruteForce::new_init(i, i), "");
         }
 
         #[cfg(feature = "parallel")]
         {
-            bench_compute_method(&bodies, &mut group, parallel::BruteForce);
-            bench_compute_method(&bodies, &mut group, parallel::BruteForceSIMD);
-            bench_compute_method(&bodies, &mut group, parallel::BarnesHut { theta: 0.5 });
+            bench_compute_method(&b, &mut gr, parallel::BruteForce, "");
+            bench_compute_method(&b, &mut gr, parallel::BruteForceSIMD, "");
+            bench_compute_method(&b, &mut gr, parallel::BarnesHut { theta: 0.2 }, "::0.2");
+            bench_compute_method(&b, &mut gr, parallel::BarnesHut { theta: 0.8 }, "::0.8");
         }
 
         {
-            bench_compute_method(&bodies, &mut group, sequential::BruteForce);
-            bench_compute_method(&bodies, &mut group, sequential::BruteForceCombinations);
-            bench_compute_method(&bodies, &mut group, sequential::BruteForceCombinationsAlt);
-            bench_compute_method(&bodies, &mut group, sequential::BruteForceSIMD);
-            bench_compute_method(&bodies, &mut group, sequential::BarnesHut { theta: 0.5 });
+            bench_compute_method(&b, &mut gr, sequential::BruteForcePairs, "");
+            bench_compute_method(&b, &mut gr, sequential::BruteForceSIMD, "");
+            bench_compute_method(&b, &mut gr, sequential::BarnesHut { theta: 0.2 }, "::0.2");
+            bench_compute_method(&b, &mut gr, sequential::BarnesHut { theta: 0.8 }, "::0.8");
         }
     }
 
-    group.finish();
+    gr.finish();
 }
 
 criterion::criterion_group!(benches, criterion_benchmark);
