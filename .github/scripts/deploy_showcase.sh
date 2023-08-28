2934c788ffaac210c5b154fb7fadfac2d021cc43
Canleskis <91223490+Canleskis@users.noreply.github.com>
2023-08-28 07:15:39 +0200

    Fix incorrect path in bash script

--- .github/scripts/deploy_showcase.sh
+++ .github/scripts/deploy_showcase.sh
@@ -1,5 +1,5 @@
 for subfolder in examples/*; do
     if [ -d $subfolder ]; then
-        ./scripts/deploy_example.sh $subfolder website/content/demos
+        ./.github/scripts/deploy_example.sh $subfolder website/content/demos
     fi
 done
