da4de495343315927d67a55cbb7607c86f11177a
Canleskis <91223490+Canleskis@users.noreply.github.com>
2023-08-30 05:14:07 +0200

    Improve deploy_example.sh script/CD

--- .github/scripts/deploy_example.sh
+++ .github/scripts/deploy_example.sh
@@ -2,30 +2,17 @@ path=$1
 out_dir=$2
 target_dir=${3:-"target"}
 
-if [[ $path = "" ]]; then
-    exit
-fi
+metadata=$(grep -A3 "\[package.metadata.particular.rs\]" $path/Cargo.toml | grep -vE "^(#|\[)")
 
-get_metadata() {
-    awk -F '= ' '/'$1'/ { print }' $path/Cargo.toml
-}
-
-demo=$(get_metadata demo | awk '{ print $3 }')
-
-if [[ $demo == true ]]; then
-    title=$(get_metadata title)
-    mobile=$(get_metadata mobile)
-    name=$(get_metadata name | awk '{ gsub(/"/, ""); print $3 }')
+if [ -n "$metadata" ]; then
+    name=$(basename $path)
 
     cargo build -p $name --release --target wasm32-unknown-unknown
-
     wasm-bindgen --no-typescript --out-name example --out-dir $out_dir/$name --target web $target_dir/wasm32-unknown-unknown/release/$name.wasm
-
     cp $path/preview.png $out_dir/$name/preview.png 2>/dev/null
     cp -r $path/assets $out_dir/$name/assets 2>/dev/null
     echo '+++
-'$title'
-'$mobile'
+'"$metadata"'
 template = "demo.html"
 +++' >$out_dir/$name/index.md
 fi
