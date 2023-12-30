8825be76d16ca74cb1ab6963ec49f562c99337a4
Canleskis <91223490+Canleskis@users.noreply.github.com>
2023-12-30 06:07:52 +0100

    Compile WASM demos with SIMD

--- .github/scripts/deploy_example.sh
+++ .github/scripts/deploy_example.sh
@@ -7,9 +7,9 @@ metadata=$(grep -A3 "\[package.metadata.particular.rs\]" $path/Cargo.toml | grep
 if [ -n "$metadata" ]; then
     name=$(basename $path)
 
-    cargo build -p $name --release --target wasm32-unknown-unknown
+    RUSTFLAGS='-C target-feature=+simd128' cargo build -p $name --release --target wasm32-unknown-unknown
     wasm-bindgen --no-typescript --out-name example --out-dir $out_dir/$name --target web $target_dir/wasm32-unknown-unknown/release/$name.wasm
-    cp $path/preview.png $out_dir/$name/preview.png 2>/dev/null
+    cp $path/preview.png $out_dir/$name/preview.png
     cp -r $path/assets $out_dir/$name/assets 2>/dev/null
     echo '+++
 '"$metadata"'
