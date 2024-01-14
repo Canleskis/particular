8cc9ea595fb2935a3ea681c35413babd80d64b2a
Canleskis <91223490+Canleskis@users.noreply.github.com>
2024-01-14 16:45:37 +0100

    Reexport `ultraviolet` types in the math module

--- particular/src/compute_method/math.rs
+++ particular/src/compute_method/math.rs
@@ -1,9 +1,8 @@
-pub use std::{
-    fmt::Debug,
+pub(crate) use std::{
     iter::Sum,
     ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
 };
-use ultraviolet::{
+pub use ultraviolet::{
     f32x4, f32x8, f64x2, f64x4, DVec2, DVec2x2, DVec2x4, DVec3, DVec3x2, DVec3x4, DVec4, DVec4x2,
     DVec4x4, Vec2, Vec2x4, Vec2x8, Vec3, Vec3x4, Vec3x8, Vec4, Vec4x4, Vec4x8,
 };
